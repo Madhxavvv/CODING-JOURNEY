1b22b8ec7961768928cce742d18ff26752093cf3
Madhav Kumar <noobmadhav1501@gmail.com>
2026-06-17 10:55:54 +0530

    Add files via upload

---
+++ firstcode.c
@@ -0,0 +1,11 @@
+#include <stdio.h> //stdio.h  is standard input output header file
+int main(){
+    printf("HELLO WORLD\n WHAT TO DO?");
+    return 0;
+}
+// /n is called escape sequence which is used to print a new line.
+/* nn
+
+nn
+nn/n/nnn/n
+*/
