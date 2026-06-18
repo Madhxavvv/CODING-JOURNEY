22b772d84a9e8907188375f3855ebfe738cabcc3
Madhav Kumar <noobmadhav1501@gmail.com>
2026-06-18 22:22:24 +0530

    Add files via upload

---
+++ typecasting.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a = 9;
+    int b = 2;
+    float c = (float)b; //type casting means changing the data type of a variable
+    printf("The value of c is %f", c);
+
+    return 0;
+}
