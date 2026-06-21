f49123fd07aca6878429c997ea416e3cfd816a50
Madhav Kumar <noobmadhav1501@gmail.com>
2026-06-21 20:55:14 +0530

    Add files via upload

---
+++ C/ternary.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a = 3, b = 2, c = 1;
+    (a > b) ? printf("A is greater") : printf("B is greater");
+    return 0;
+}
+
+// condition1 ? statement if true : statement if false
