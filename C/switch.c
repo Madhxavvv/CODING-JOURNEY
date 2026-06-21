212b7fc097ebe6ec6698aa60cb2429393b4d46e5
Madhav Kumar <noobmadhav1501@gmail.com>
2026-06-21 20:57:05 +0530

    Add files via upload

---
+++ C/switch.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a: ");
+    scanf("%d", &a);
+    switch (a)
+    {
+    case 1:
+        printf("You entered 1\n");
+        break;
+    case 2:
+        printf("You entered 2\n");
+        break;
+    default:
+       printf("NOTHING MATCHED");
+       break;
+    }
+    return 0;
+}
+
+
+
