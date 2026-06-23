0daa6791ac7cda97d8416475ad2f1c920b4d46da
Madhav Kumar <noobmadhav1501@gmail.com>
2026-06-23 21:41:48 +0530

    Add files via upload

---
+++ C/for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int i;
+    i = 0;
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    for(i=1;i<=n;i++){
+        printf("%d\n", i);
+    }
+    return 0;
+}
