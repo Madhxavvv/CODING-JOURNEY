2e6f9b4249aa4873ee9393957e2cb449647fa92f
Madhav Kumar <noobmadhav1501@gmail.com>
2026-06-23 16:06:27 +0530

    Add files via upload

---
+++ C/while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+    int i;
+    i = 0;
+    while(i<4){
+        printf("Happy Birthday!!\n");
+        i++;
+    }
+    return 0;
+}
+
+// {
+//         printf("Happy Birthday!!\n");
+//         i++;
+//     }
+
+// This block will be executed till the condition in while is true!!
+// If the condition in while is always true then it is an infinite loop!!
