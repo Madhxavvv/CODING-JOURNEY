60e218e09a522e8445b6f3cc34c50325c3006103
Madhav Kumar <madhavjee2026@outlook.com>
2026-06-30 23:29:31 +0530

    Update 13_pointer_arithmetics.c

--- C/13_pointer_arithmetics.c
+++ C/13_pointer_arithmetics.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
     int i = 72;
-    int *ptr = &i;
+    int *ptr = &i; // storing the address of i in pointer
     printf("The address of i is %u\n", ptr);
-    printf("The value at address of i is %d\n", *ptr);
+    printf("The value at address of i is %d\n", *ptr); 
     ptr++; // INCREMENT THE ADDRESS TO NEXT BOX ,i.e, if int variable so next memory box is after 4 bytes 
     printf("The value of address is now %u\n", ptr);
     ptr--; // DECREMENT THE ADDRESS TO PREVIOUS BOX , i.e, if int variable so previous memory box is before 4 bytes
