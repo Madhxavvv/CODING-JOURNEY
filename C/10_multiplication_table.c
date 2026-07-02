96f200ee9a36b9dcc9336c81ff84b8b544d69fcd
Madhav Kumar <madhavjee2026@outlook.com>
2026-07-02 13:46:49 +0530

    Update 10_multiplication_table.c

--- C/10_multiplication_table.c
+++ C/10_multiplication_table.c
@@ -7,7 +7,7 @@ int main(){
     printf("Enter value of n: ");
     scanf("%d", &n);
     for(int i = 1; i<=10; i++){
-        product = n*i;
+        int product = n*i;
         printf("%d\n", product);
     }
     return 0;
